8061461fcbd37a215738c3c2ced97fa05530f7b8
Alex <smsxander@gmail.com>
2020-07-27 18:07:19 +0200

    installation bootswatch

--- src/App.vue
+++ src/App.vue
@@ -14,16 +14,14 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
 #nav {
-  padding: 10px;
+  padding: 30px;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
