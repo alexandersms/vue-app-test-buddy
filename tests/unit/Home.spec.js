9f84ad14e37f597839184eb0aad63ae3c7bf7a3a
Alex <smsxander@gmail.com>
2020-07-27 12:51:45 +0200

    add unit test

--- tests/unit/Home.spec.js
+++ tests/unit/Home.spec.js
@@ -1,20 +1,21 @@
-import { expect } from "chai"
+//import { expect } from "chai"
 import { shallowMount } from "@vue/test-utils";
 import Home from "@/views/Home.vue";
 
 describe("Home.vue", () => {
   it("h1 has correct h1 tag rendered", () => {
     const wrapper = shallowMount(Home);
-    expect(wrapper.find("h1").text()).equal("Random Counter")
+    expect(wrapper.find("h1").text()).toMatch("Random Counter")
   });
 
   it("button has correct text rendered", () => {
     const wrapper = shallowMount(Home);
-    expect(wrapper.find("#increment").text()).equal("Increment")
+    expect(wrapper.find("#increment").text()).toMatch("Increment")
   });
 
   it("button has correct text rendered", () => {
     const wrapper = shallowMount(Home);
-    expect(wrapper.find("#decrement").text()).equal("Decrement")
+    expect(wrapper.find("#decrement").text()).toMatch("Decrement")
   });
+  
 })
